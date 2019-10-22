2813086e6a188ac49ccb66e5196bd671905c3bcb
TheTraveller95 <crivellaroandres@gmail.com>
2019-10-22 19:22:19 +0000

    if statement

--- script.js
+++ script.js
@@ -27,5 +27,30 @@ $(document).ready(function() {
 	  	$(".stream3").addClass('highlight_stream');
 	});
 
+    $("#button1").mouseenter(function(){
+        $("#button1").fadeTo('slow',0.5)
+    })
+
+    $("#button1").mouseleave(function(){
+        $("#button1").fadeTo('slow',1)
+    })
+
+    $("#button1").click(function(){
+        $("#p_button1").slideToggle('3000')
+    })
+
+    $(".card").click(function(){
+        var className = $(this).attr("class").split(" ");
+        var card = className[0];
+        var stream = className[1];
+
+        if($(this).css("background-color") == "#fff"){
+            $("."+className[1]).css("background-color", "red")
+
+        } else {
+             $("."+className[1]).css("background-color", "white");
+        }
+
+    })
 
 });
